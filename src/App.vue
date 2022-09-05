63ad874064761b188f5fdd54d097cf8c2db19878
Ivan Che <ivan.chepurin@gmail.com>
2022-09-05 18:54:52 +0300

    prepare test task

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="main">
+  </div>
+</template>
+
+<style scoped lang="scss">
+.main {
+  background: $dark;
+  width: 100%;
+  height: 100vh;
+  font-size: 20px;
+  font-family: sans-serif;
+  text-transform: uppercase;
+}
+</style>
